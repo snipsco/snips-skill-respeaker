3f861c8898a52598f588669526e25a15a3650de4
Charles <nebuto@users.noreply.github.com>
2018-06-14 12:01:06 +0200

    Remove print of arecord

--- snipsrespeaker/snipsrespeaker.py
+++ snipsrespeaker/snipsrespeaker.py
@@ -119,7 +119,6 @@ class SnipsRespeaker:
         GPIO.setmode(GPIO.BCM)
         p = subprocess.Popen(['arecord', '-l'], stdout=subprocess.PIPE)
         out, err = p.communicate()
-        print(out)
         if (out.find("seeed-4mic-voicecard") != -1):
             num_led = 12
             GPIO.setup(5, GPIO.OUT, initial=GPIO.HIGH)
