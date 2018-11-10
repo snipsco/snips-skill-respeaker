f8e8cb1a555491cecc33967ec2e9c95eb0fe925a
CoorFun <coorfang@gmail.com>
2018-11-10 17:01:13 +0100

    [Update] correct indentation

--- include/common.h
+++ include/common.h
@@ -10,25 +10,25 @@
 #include <fcntl.h>
 #include <pthread.h>
 
-#define NUM_TOPIC			10
-#define HOT_ON				"hermes/hotword/toggleOn"
-#define HOT_OFF				"hermes/hotword/toggleOff"
-#define STA_LIS 			"hermes/asr/startListening"
-#define END_LIS 			"hermes/asr/stopListening"
-#define STA_SAY 			"hermes/tts/say"
-#define END_SAY 			"hermes/tts/sayFinished"
-#define SUD_ON  			"hermes/feedback/sound/toggleOn"
-#define SUD_OFF 			"hermes/feedback/sound/toggleOff"
-#define LED_ON  			"hermes/feedback/led/toggleOn"
-#define LED_OFF 			"hermes/feedback/led/toggleOff"
+#define NUM_TOPIC				10
+#define HOT_ON					"hermes/hotword/toggleOn"
+#define HOT_OFF					"hermes/hotword/toggleOff"
+#define STA_LIS					"hermes/asr/startListening"
+#define END_LIS					"hermes/asr/stopListening"
+#define STA_SAY					"hermes/tts/say"
+#define END_SAY					"hermes/tts/sayFinished"
+#define SUD_ON					"hermes/feedback/sound/toggleOn"
+#define SUD_OFF					"hermes/feedback/sound/toggleOff"
+#define LED_ON					"hermes/feedback/led/toggleOn"
+#define LED_OFF					"hermes/feedback/led/toggleOff"
 
-#define STATE_NUM 				6
-#define ON_IDLE_STR 			"on_idle"
-#define ON_LISTEN_STR 		"on_listen"
-#define ON_SPEAK_STR 			"on_speak"
-#define TO_MUTE_STR 			"to_mute"
-#define TO_UNMUTE_STR 		"to_unmute"
-#define ON_DISABLED_STR		"on_disabled"
+#define STATE_NUM				6
+#define ON_IDLE_STR			"on_idle"
+#define ON_LISTEN_STR		"on_listen"
+#define ON_SPEAK_STR		"on_speak"
+#define TO_MUTE_STR			"to_mute"
+#define TO_UNMUTE_STR		"to_unmute"
+#define ON_DISABLED_STR	"on_disabled"
 
 typedef enum {
 	ON_IDLE = 0,
