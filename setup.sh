616e6dcb46b351efd68d081aa9d3074b789e19c3
CoorFun <coorfang@gmail.com>
2019-01-11 14:54:05 +0100

    [Change] make the setup more readable

--- setup.sh
+++ setup.sh
@@ -23,17 +23,7 @@ case $R in
 		;;
 esac
 
-echo -e "\033[1;32;32m[*]\033[m Detected hardware model: \033[1;32m[$MODEL]\033[m"
-
-# read a option value by its key from config.ini
-function ReadINIfile()
-{
-	Key=$1
-	Section=$2
-  	Configfile=$3
-	ReadINI=`awk -F '=' '/\['$Section'\]/{a=1}a==1&&$1~/'$Key'/{print $2;exit}' $Configfile`
- 	echo "$ReadINI"
-}
+echo -e "\033[1;32;34m[*]\033[m Detected hardware model: \033[0;35m<$MODEL>\033[m"
 
 # user config version checking
 if [ ! -e $CONFIG_FILE ]
@@ -41,40 +31,45 @@ then
     cp config.ini.default config.ini
 	sed -i s/model=.*\$/model=$MODEL/g config.ini
 else
-    user_ver=`ReadINIfile "config_ver" "static" "$CONFIG_FILE"`
-    def_ver=`ReadINIfile "config_ver" "static" "$DEFAULT_CONFIG_FILE"`
+	user_ver=`cat $CONFIG_FILE | grep "config_ver" | sed 's/^config_ver=\([0-9]\.[0-9]\)/\1/g'`
+	def_ver=`cat $DEFAULT_CONFIG_FILE | grep "config_ver" | sed 's/^config_ver=\([0-9]\.[0-9]\)/\1/g'`
+
     if [ "$def_ver" != "$user_ver" ]
     then
         echo -e "\033[1;32;31m[!]\033[m Old config options are overwrote by the new default value since they are too old"
-        echo -e "\033[1;32;31m[*]\033[m The lastest config.ini version is \033[1;32m[$def_ver]\033[m"
+        echo -e "\033[1;32;31m[*]\033[m The lastest config.ini version is \033[0;35m<$def_ver>\033[m"
         echo -e "\033[1;32;31m[*]\033[m Please change it manually to adapt to your old setup after installation"
-        echo -e "\033[1;32;31m[*]\033[m Press any key to confirm this change"
-        read -s -n1 -p "\033[1;32;31m[*]\033[m ..."
+        echo -e "\033[1;32;31m[*]\033[m Press any key to confirm this change\c"
+        read -s -n1 -p "..."
         echo ""
         cp config.ini.default config.ini
 		sed -i s/model=.*\$/model=$MODEL/g config.ini
+	else
+		echo -e "\033[1;32;34m[*]\033[m Good config.ini version: \033[0;35m<$user_ver>\033[m"
     fi
 fi
 
+echo -e "\033[1;32;34m[*]\033[m Cleaning... \c"
 # check if the old compiled file is outputed
 if [ -e $ACTION_FILE ]
 then
     make clean
 fi
 
-echo "=============================="
-echo -e "\033[1;32;32m[*]\033[m Compiling... "
+echo -e "\033[1;32;34m[*]\033[m Compiling... "
+echo -e "\033[1;32;34m==============================\033[m"
+
 # compile application
 make all
 
-echo "=============================="
+echo -e "\033[1;32;34m==============================\033[m"
 
 # check if the compiled file is outputed
 if [ -e $ACTION_FILE ]
 then
-    echo -e "\033[1;32;32m[*]\033[m Setup Success!"
+    echo -e "\033[1;32;34m[*]\033[m Setup Success!"
+	echo -e "\033[1;32;34m[*]\033[m \c"
+	./action-led_animation_* --version
 else
     echo -e "\033[1;32;31m[!]\033[m Setup Failed!"
 fi
-
-./action-led_animation_* --version
