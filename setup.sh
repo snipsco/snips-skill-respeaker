04302fcfde7363165ec0d0749bff573ba0b04d85
CoorFun <coorfang@gmail.com>
2019-08-05 15:03:57 +0200

    [Change] Removed -e and CLI color control

--- setup.sh
+++ setup.sh
@@ -24,19 +24,19 @@ case $OS in
                 ;;
             *)
                 MODEL="NONE"
-                echo -e "\033[1;32;31m[!]\033[mDid not found any official supported hardware"
-                echo -e "\033[1;32;31m[*]\033[mPlease refer to README.md, manually create your hardware specification"
+                echo "[!] Did not found any official supported hardware"
+                echo "[*] Please refer to README.md, manually create your hardware specification"
                 ;;
         esac
         ;;
     *)
         MODEL="NONE"
-        echo -e "\033[1;32;31m[!]\033[mDid not found any official supported hardware"
-        echo -e "\033[1;32;31m[*]\033[mPlease refer to README.md, manually create your hardware specification"
+        echo "[!] Did not found any official supported hardware"
+        echo "[*] Please refer to README.md, manually create your hardware specification"
         ;;
 esac
 
-echo -e "\033[1;32;34m[*]\033[m Detected hardware model: \033[0;35m<$MODEL>\033[m"
+echo "[*] Detected hardware model: <$MODEL>"
 
 # user config version checking
 if [ ! -e $CONFIG_FILE ]
@@ -48,12 +48,12 @@ else
 
     if [ "$def_ver" != "$user_ver" ]
     then
-        echo -e "\033[1;32;31m[!]\033[m Current config options are overwrote by the new default value since they are out of date"
-        echo -e "\033[1;32;31m[*]\033[m The lastest config.ini version is \033[0;35m<$def_ver>\033[m"
-        echo -e "\033[1;32;31m[*]\033[m Please change it manually to adapt to your old setup after installation"
+        echo "[!] Current config options are overwrote by the new default value since they are out of date"
+        echo "[*] The lastest config.ini version is <$def_ver>"
+        echo "[*] Please change it manually to adapt to your old setup after installation"
         cp config.ini.default config.ini
     else
-        echo -e "\033[1;32;34m[*]\033[m Good config.ini version: \033[0;35m<$user_ver>\033[m"
+        echo "[*] Good config.ini version: <$user_ver>"
     fi
 fi
 
@@ -62,17 +62,17 @@ sed -i s/model=.*\$/model=$MODEL/g config.ini
 # check if the old compiled file is outputed
 if [ "$ACTION_FILE" != "0" ]
 then
-    echo -e "\033[1;32;34m[*]\033[m Cleaning... "
+    echo "[*] Cleaning... "
     rm action-*
 fi
 
-echo -e "\033[1;32;34m[*]\033[m Compiling... "
-echo -e "\033[1;32;34m==============================\033[m"
+echo "[*] Compiling... "
+echo "=============================="
 
 # compile application
 make all
 
-echo -e "\033[1;32;34m==============================\033[m"
+echo "=============================="
 
 # check if snips user has the permission to execute the action code
 P_SPI=`groups _snips-skills | grep spi`
@@ -80,25 +80,25 @@ P_GPIO=`groups _snips-skills | grep gpio`
 
 if [ -z "$P_SPI" ]
 then
-    echo -e "\033[1;32;31m[!]\033[m SPI operating permission required, please manually enable it!"
-    echo -e "\033[1;32;31m[-]\033[m Run the following command on your snips device:"
-    echo -e "\033[1;32;31m[-]\033[m     \033[0;35msudo usermod -a -G spi _snips-skills\033[m"
+    echo "[!] SPI operating permission required, please manually enable it!"
+    echo "[-] Run the following command on your snips device:"
+    echo "[-]     sudo usermod -a -G spi _snips-skills"
 fi
 
 if [ -z "$P_GPIO" ]
 then
-    echo -e "\033[1;32;31m[!]\033[m GPIO operating permission required, please manually enable it!"
-    echo -e "\033[1;32;31m[-]\033[m Run the following command on your snips device:"
-    echo -e "\033[1;32;31m[-]\033[m     \033[0;35msudo usermod -a -G gpio _snips-skills\033[m"
+    echo "[!] GPIO operating permission required, please manually enable it!"
+    echo "[-] Run the following command on your snips device:"
+    echo "[-]     sudo usermod -a -G gpio _snips-skills"
 fi
 
 ACTION_FILE=$(ls action-* 2> /dev/null | wc -l)
 # check if the compiled file is outputed
 if [ "$ACTION_FILE" != "0" ]
 then
-    echo -e "\033[1;32;34m[*]\033[m Setup Finished!"
-    echo -e "\033[1;32;34m[*]\033[m \c"
+    echo "[*] Setup Finished!"
+    echo "[*] \c"
     ./action-led_animation_* --version
 else
-    echo -e "\033[1;32;31m[!]\033[m Setup Failed!"
+    echo "[!] Setup Failed!"
 fi
